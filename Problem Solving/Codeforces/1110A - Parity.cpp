019e0ecdb1ebea60023ca58a5ebb5b0ea0074bb0
Khaled-Farhat <46577177+Khaled-Farhat@users.noreply.github.com>
2019-02-07 22:18:02 +0300

    Update 1110A - Parity.cpp

--- Problem Solving/Codeforces/1110A - Parity.cpp
+++ Problem Solving/Codeforces/1110A - Parity.cpp
@@ -9,18 +9,21 @@
 #include <cmath>
 using namespace std;
 
-int idx, a[100001], k, b;
-unsigned long long n=0;
+
+    int a[100001], k, b;
+    unsigned long long n=0;
+
 
 int main()
 {
     cin >> b >> k;
-    idx=k;
 
     for (int i=0; i<k; i++)
         cin >> a[i];
 
+    
     unsigned long long p=1;
+    
     for (int i=k-1; i>=0; i--)
     {
         n+=a[i]*p;
@@ -28,8 +31,10 @@ int main()
         p*=b;
     }
 
+    
     if (n%2==0)     cout << "even";
     else            cout << "odd";
 
+    
     return 0;
 }
