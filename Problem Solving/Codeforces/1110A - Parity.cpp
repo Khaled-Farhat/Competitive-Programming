d73cbfd0eff956ad1e7e36840eff3be590dc0c7c
Khaled-Farhat <46577177+Khaled-Farhat@users.noreply.github.com>
2019-02-07 22:09:17 +0300

    Create 1110A - Parity.cpp

---
+++ Problem Solving/Codeforces/1110A - Parity.cpp
@@ -0,0 +1,35 @@
+/**
+ *  created on: 09.01.2019
+ *  author: Khaled Farhat
+**/
+
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int idx, a[100001], k, b;
+unsigned long long n=0;
+
+int main()
+{
+    cin >> b >> k;
+    idx=k;
+
+    for (int i=0; i<k; i++)
+        cin >> a[i];
+
+    unsigned long long p=1;
+    for (int i=k-1; i>=0; i--)
+    {
+        n+=a[i]*p;
+
+        p*=b;
+    }
+
+    if (n%2==0)     cout << "even";
+    else            cout << "odd";
+
+    return 0;
+}
